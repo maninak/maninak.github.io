1a52705fd98dddf82297394d9d34a55666dae76e
Kostis Maninakis <kmaninak@gmail.com>
2017-06-09 23:14:32 +0300

    refactor(javascript): move from inline with HTML into external js file

---
+++ assets/js/main.js
@@ -0,0 +1,78 @@
+/* GOOGLE ANALYTICS */
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-70469459-6', 'auto');
+ga('send', 'pageview');
+
+function gaEvent(clickedItem){
+  ga('send', 'event', clickedItem, 'click');
+}
+
+
+/* INITIALISATION */
+window.onload = function() { document.body.className = ''; }
+window.ontouchmove = function() { return false; }
+window.onorientationchange = function() { document.body.scrollTop = 0; }
+
+
+/* BG ANIMATION */
+setInterval( function (){
+  updateGradient();
+}, 50);
+
+var colors = new Array(
+  [89,   97, 100],
+  [83,  120, 149],
+  [22,  160, 130],
+  [255, 154,  68]
+);
+
+var step = 0;
+//color table indices for: 
+// current color left
+// next color left
+// current color right
+// next color right
+var colorIndices = [0,1,2,3];
+
+//transition speed
+var gradientSpeed = 0.01;
+
+function updateGradient() {          
+  var c0_0 = colors[colorIndices[0]];
+  var c0_1 = colors[colorIndices[1]];
+  var c1_0 = colors[colorIndices[2]];
+  var c1_1 = colors[colorIndices[3]];
+
+  var istep = 1 - step;
+  var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+  var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+  var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+  var color1 = "rgb("+r1+","+g1+","+b1+")";
+
+  var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+  var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+  var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+  var color2 = "rgb("+r2+","+g2+","+b2+")";
+
+  let gradientEl = document.getElementsByClassName("gradient")[0];
+  gradientEl.style.background = "transparent -moz-linear-gradient(0% 0% , " + color1+ " 0%, " 
+      + color2 + " 100%) repeat scroll 0% 0%";
+  gradientEl.style.background = "-webkit-gradient(linear, 0% 0%, 100% 100%, from(" + color1 + "), to(" 
+      + color2 + "))";
+    
+  step += gradientSpeed;
+  if ( step >= 1 ){
+    step %= 1;
+    colorIndices[0] = colorIndices[1];
+    colorIndices[2] = colorIndices[3];
+    
+    //pick two new target color indices
+    //do not pick the same as the current one
+    colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+    colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+  }
+}
