d0343af49a9aaddfb04453e44b50e1af81dbe223
Kostis Maninakis <kmaninak@gmail.com>
2017-06-12 02:21:46 +0300

    refactor(syntax): use let for declarations instead of var

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,40 +23,40 @@ setInterval( function (){
   updateGradient();
 }, 50);
 
-var colors = new Array(
+let colors = new Array(
   [89,   97, 100],
   [83,  120, 149],
   [22,  160, 130],
   [255, 154,  68]
 );
 
-var step = 0;
+let step = 0;
 //color table indices for: 
 // current color left
 // next color left
 // current color right
 // next color right
-var colorIndices = [0,1,2,3];
+let colorIndices = [0,3,2,1];
 
 //transition speed
-var gradientSpeed = 0.01;
+let gradientSpeed = 0.01;
 
 function updateGradient() {          
-  var c0_0 = colors[colorIndices[0]];
-  var c0_1 = colors[colorIndices[1]];
-  var c1_0 = colors[colorIndices[2]];
-  var c1_1 = colors[colorIndices[3]];
+  let c0_0 = colors[colorIndices[0]];
+  let c0_1 = colors[colorIndices[1]];
+  let c1_0 = colors[colorIndices[2]];
+  let c1_1 = colors[colorIndices[3]];
 
-  var istep = 1 - step;
-  var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
-  var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
-  var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
-  var color1 = "rgb("+r1+","+g1+","+b1+")";
+  let istep = 1 - step;
+  let r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+  let g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+  let b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+  let color1 = "rgb("+r1+","+g1+","+b1+")";
 
-  var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
-  var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
-  var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
-  var color2 = "rgb("+r2+","+g2+","+b2+")";
+  let r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+  let g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+  let b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+  let color2 = "rgb("+r2+","+g2+","+b2+")";
 
   let gradientEl = document.getElementsByClassName("gradient")[0];
     
